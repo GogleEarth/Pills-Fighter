1ad6d1742b0d19fbd5ed3b896d505c59b2e95456
GogleEarth <32813163+GogleEarth@users.noreply.github.com>
2018-10-14 21:55:50 +0900

    서버
    서버 기본

---
+++ Project/PillsServer/PillsServer/main.cpp
@@ -0,0 +1,122 @@
+#pragma comment(lib, "ws2_32")
+
+#include <WinSock2.h>
+#include <iostream>
+#include <stdlib.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXCollision.h>
+
+#define SERVERPORT	9000
+#define FLAG		FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM
+#define LANG		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT)
+#define WS22		MAKEWORD(2,2)
+void err_quit(char* msg);
+void err_display(char* msg);
+int recvn(SOCKET s, char* buf, int len, int flags);
+
+struct PLAYER_INFO
+{
+	int client_id;
+	DirectX::XMFLOAT4X4 xmf4x4World;
+};
+
+int main()
+{
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(WS22, &wsa) != 0)
+		return 1;
+
+	// socket()
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET)
+		err_quit("socket()");
+
+	// bind()
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR)
+		err_quit("bind()");
+
+	// listen()
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR)
+		err_quit("listen()");
+
+	// 데이터 통신에 사용될 변수
+	SOCKET client_sock;
+	SOCKADDR_IN clientaddr;
+	int addrlen;
+
+	while (true)
+	{
+		// accept()
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
+		if (client_sock == INVALID_SOCKET)
+		{
+			err_display("accept()");
+			break;
+		}
+	}
+
+	// closesocket()
+	closesocket(listen_sock);
+
+	// 윈속 종료
+	WSACleanup();
+	
+	return 0;
+}
+
+void err_quit(char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FLAG, NULL, 
+		WSAGetLastError(), LANG, 
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(
+		NULL, (LPCTSTR)lpMsgBuf, 
+		msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+void err_display(char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FLAG, NULL,
+		WSAGetLastError(), LANG,
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s", msg, (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+int recvn(SOCKET s, char * buf, int len, int flags)
+{
+	int received;
+	char* ptr = buf;
+	int left = len;
+
+	while (left > 0)
+	{
+		received = recv(s, ptr, left, flags);
+		if (received == SOCKET_ERROR)
+			return SOCKET_ERROR;
+		else if (received == 0)
+			break;
+		left -= received;
+		ptr += received;
+	}
+
+	return (len - left);
+}
