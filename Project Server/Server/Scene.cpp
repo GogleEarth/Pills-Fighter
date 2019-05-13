f52b555ca14f158ef886058a7dde19919e9b698c
daehun kim <dkekfps@naver.com>
2019-05-13 13:29:34 +0900

    서버수정
    건물뺌

--- Project Server/Server/Scene.cpp
+++ Project Server/Server/Scene.cpp
@@ -41,11 +41,11 @@ void CScene::BuildObjects(CRepository* pRepository)
 	m_vpModels.emplace_back(pRepository->GetModel("./Resource/Buildings/Wall.bin", NULL, NULL));
 	InsertObjectFromLoadInfFromBin("./Resource/Buildings/WallSelfData.bin", 7);
 
-	m_vpModels.emplace_back(pRepository->GetModel("./Resource/Buildings/Brick_Garage.bin", NULL, NULL));
-	InsertObjectFromLoadInfFromBin("./Resource/Buildings/B_BrickGarageSelfData.bin", 8);
+	//m_vpModels.emplace_back(pRepository->GetModel("./Resource/Buildings/Brick_Garage.bin", NULL, NULL));
+	//InsertObjectFromLoadInfFromBin("./Resource/Buildings/B_BrickGarageSelfData.bin", 8);
 
 	m_vpModels.emplace_back(pRepository->GetModel("./Resource/Buildings/fence.bin", NULL, NULL));
-	InsertObjectFromLoadInfFromBin("./Resource/Buildings/FenceSelfData.bin", 9);
+	InsertObjectFromLoadInfFromBin("./Resource/Buildings/FenceSelfData.bin", 8);
 
 	m_pGMMesh = pRepository->GetModel("./Resource/PlayerCollisionBox.bin", NULL, NULL);
 	m_pBulletMesh = pRepository->GetModel("./Resource/Bullet/Bullet.bin", NULL, NULL);
