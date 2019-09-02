084ea5fa7c6304f615efcef46a4c9451130ec174
GogleEarth <32813163+GogleEarth@users.noreply.github.com>
2019-09-02 09:20:53 +0900

    서버수정

--- Project_Server_IOCP/Project_Server_IOCP/Project_Server_IOCP.cpp
+++ Project_Server_IOCP/Project_Server_IOCP/Project_Server_IOCP.cpp
@@ -5,15 +5,15 @@ int main()
 {
 	std::wcout.imbue(std::locale("korean"));
 
-	Framawork framework;
+	Framawork* framework = new Framawork();
 	std::vector<std::thread> worker_threads;
 
-	framework.init();
+	framework->init();
 	
 	for (int i = 0; i < 4; ++i)
-		worker_threads.emplace_back(std::thread{ [&]() { framework.thread_process(); } });
-	std::thread accept_thread{ [&]() { framework.accept_process(); } };
-	std::thread timer_thread{ [&]() { framework.timer_process(); } };
+		worker_threads.emplace_back(std::thread{ [&]() { framework->thread_process(); } });
+	std::thread accept_thread{ [&]() { framework->accept_process(); } };
+	std::thread timer_thread{ [&]() { framework->timer_process(); } };
 
 	timer_thread.join();
 	accept_thread.join();
